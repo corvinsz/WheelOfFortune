928a13ae400def007e3f404d40c2a54808f39507
Corvin <corvinszimion@googlemail.com>
2025-11-01 21:17:59 +0100

    add UI for SettingsDialog

---
+++ WheelOfFortune/Dialogs/SettingsDialog.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace WheelOfFortune.Dialogs;
+
+/// <summary>
+/// Interaction logic for SettingsDialog.xaml
+/// </summary>
+public partial class SettingsDialog : UserControl
+{
+	public SettingsDialog()
+	{
+		InitializeComponent();
+	}
+}
