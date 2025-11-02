17940ef22102f9054609ac570a686bc4284c0536
Corvin <corvinszimion@googlemail.com>
2025-11-02 11:43:45 +0100

    add script to build installer with velopack

---
+++ WheelOfFortune/build.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+
+if "%~1"=="" (
+    echo Version number is required.
+    echo Usage: build.bat [version] [extra_args...]
+    exit /b 1
+)
+
+set "version=%~1"
+
+echo.
+echo Compiling WheelOfFortune with dotnet...
+dotnet publish -c Release --no-self-contained -r win-x64 -o %~dp0publish
+
+echo.
+echo Building Velopack Release v%version%
+vpk pack -u WheelOfFortune -o %~dp0releases -p %~dp0publish -f net9-x64-desktop -v %*
